f75836a95dcb7c93caccf3c6a6d231372a78da7f
Syed Ahmad Alisha <ahmadalishasyed@gmail.com>
2025-12-14 12:50:24 +0530

    04 : Dashboard Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Logout from '@/module/auth/components/logout'
 import { requireAuth } from '@/module/auth/utils/auth-utils'
+import { redirect } from 'next/navigation'
 
 
 const page = async () => {
   await requireAuth()
-  return (
-    <div className='flex items-center justify-center h-screen'>
-      <Logout>
-        <Button>Logout</Button>
-      </Logout>
-    </div>
-  )
+  return redirect('/dashboard')
 }
 
 export default page
